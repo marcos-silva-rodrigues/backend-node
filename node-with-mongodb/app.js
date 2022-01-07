1fb31f299d76efcbd0cfbba7f90742347b29eb1c
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-07 07:45:08 -0300

    feat(node-with-mongodb): removendo rotas e models

--- node-with-mongodb/app.js
+++ node-with-mongodb/app.js
@@ -22,8 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/person', personRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
