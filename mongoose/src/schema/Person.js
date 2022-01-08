0ca5e0f089b33feeceebc3f19244b638768011fc
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-08 16:57:44 -0300

    feat(mongoose): criando schema

---
+++ mongoose/src/schema/Person.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Person = mongoose.Schema({
+  name: {
+    firstname: String,
+    lastname: String,
+  },
+  age: {
+    type: Number, 
+  }
+});
+
+module.exports = mongoose.model('Person', Person);
