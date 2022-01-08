a033c44bd4592e71c67a3992439442f0422a548f
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-08 14:18:45 -0300

    feat(node-with-mongodb): finalizando crud

--- node-with-mongodb/routes/cars.js
+++ node-with-mongodb/routes/cars.js
@@ -4,7 +4,12 @@ const carService = require('../services/cars');
 const router = express.Router();
 
 router.get('/', carService.find);
+router.post('/', carService.create);
+router.put('/:id', carService.update);
+router.delete('/:id', carService.remove);
+
 router.get('/new', carService.new);
 router.get('/edit/:id', carService.edit);
+
 
 module.exports = router;
--- node-with-mongodb/services/cars.js
+++ node-with-mongodb/services/cars.js
@@ -1,11 +1,58 @@
-exports.find = (req, res) => {
-  res.render('cars_list', {});
+const Car = require('../models/car');
+
+exports.find = async (req, res) => {
+  const carsList = await Car.find({});
+  res.render('cars_list', {
+    cars: carsList,
+  });
 }
 
 exports.new = (req, res) => {
   res.render('cars_new', {});
 }
 
-exports.edit = (req, res) => {
-  res.render('cars_edit', {});
+exports.create = async (req, res) => {
+  try {
+    const cardCreated = await Car.create(req.body);
+
+    return res.redirect('/cars');
+  } catch (error) {
+    return;
+  };
+};
+
+exports.edit = async (req, res) => {
+  try {
+    const hasCar = await Car.findById(req.params.id);
+
+    return res.render('cars_edit', {
+      car: hasCar
+    });
+  } catch (error) {
+    return;
+  }
+
 }
+
+exports.update = async (req, res) => {
+  try {
+    const carUpdated = await Car.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+  
+    res.redirect('/cars');
+  } catch {
+    return;
+  };
+};
+
+exports.remove = async (req, res) => {
+  try {
+    const carUpdated = await Car.findByIdAndRemove(req.params.id);
+  
+    res.redirect('/cars');
+  } catch {
+    return;
+  };
+};
