fd87019a3a46e62a3a17c43d8641db40a31c89e1
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-09 14:03:29 -0300

    feat(init-test): criando endpoint

---
+++ init-tests/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+import routes from './routes';
+
+const app = express();
+
+app.use(morgan());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+routes(app);
+
+app.listen(3000, () => {
+  console.log("Express has been started ...");
+});
---
+++ init-tests/src/routes.js
@@ -0,0 +1,5 @@
+import binary from './binary';
+
+export default (app) => {
+  app.use('/binary', binary);
+}
