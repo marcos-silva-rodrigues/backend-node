0119e4e036f402b665019994af28ed3127533f40
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-09 14:54:24 -0300

    feat(init-tests): rota de erro

---
+++ init-tests/src/error/index.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post('/', (req, res) => {
+  return res
+    .status(500)
+    .json({
+      msg: 'Internal server error',
+      error: {}
+    });
+});
+
+export default router;
