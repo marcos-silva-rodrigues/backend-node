2f12757f73db41a7c5c25903e1406ab473012cb6
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-09 14:33:06 -0300

    feat(init-tests): classe calculator

---
+++ init-tests/src/Calculator.js
@@ -0,0 +1,20 @@
+export default class Calculator {
+  constructor() {};
+
+  sum(num1, num2) {
+    return num1 + num2;
+  }
+
+  minus(num1, num2) {
+    return num1 - num2;
+  }
+
+  mult(num1, num2) {
+    return num1 * num2;
+  }
+
+  div(num1, num2) {
+    return num1 / num2;
+  }
+
+};
