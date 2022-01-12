0ce1c95f026b31599651ccb0474904074490316e
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-11 23:49:46 -0300

    feat(redis): criando conexão com o redis e cacheando dados

--- redis/src/tasks/list.js
+++ redis/src/tasks/list.js
@@ -1,10 +1,22 @@
 const Task = require('../schemas/Task');
+const clientRedis = require('../redis');
 
 module.exports = async (req, res) => {
   try {
+    await clientRedis.connect();
+    const cached = await clientRedis.get('tasks');
+    if (cached) {
+      return res.status(200).json({ data: cached })
+    }
     const data = await Task.find({});
+      
+    await clientRedis.set('tasks', JSON.stringify(data), {EX: 60 });
+
     return res.status(200).json({ data });
+
   } catch (err) {
     return res.status(500).json({ err })
+  } finally {
+    await clientRedis.quit();
   }
 }
