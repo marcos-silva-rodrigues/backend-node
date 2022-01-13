5ce940d2e021392746a6fb6f9e95884824a238f6
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-12 21:23:53 -0300

    feat(typeorm): iniciando com repository e entityManager

---
+++ typeorm/src/entity/Task.ts
@@ -0,0 +1,15 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class Task {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    done: boolean;
+
+}
--- typeorm/src/index.ts
+++ typeorm/src/index.ts
@@ -1,21 +1,18 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { createConnection, getConnection, getRepository } from "typeorm";
+import { Task } from "./entity/Task";
 
-createConnection().then(async connection => {
+(async () => {
+  try {
+    await createConnection();
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    // const entityManager = getConnection().manager;
+    // const res = await entityManager.findAndCount(Task);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+    const taskRepository = getRepository(Task);
+    const res = await taskRepository.findAndCount();
+    console.log(res);
+  } catch (ex) {
+    console.error('Error:', ex);
+  }
+})();
