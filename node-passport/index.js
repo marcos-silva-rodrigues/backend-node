e0cdb53eb66e4b468ff81f91db124e3589ae6a92
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-15 16:33:04 -0300

    feat(node-passport): basic strategy

--- node-passport/index.js
+++ node-passport/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const passport = require('passport');
 
-
+/* PASSPORT BASIC */
+passport.use(require('./src/auth/basic'));
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(passport.initialize())
 
+app.get('*', passport.authenticate('basic', { session: false }));
 require('./src/index')(app);
 
 app.listen(3000, () => {
